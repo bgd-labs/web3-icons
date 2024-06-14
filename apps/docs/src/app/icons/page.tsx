2bdbf1d8144dbdca63bd0a0971a4658ceff1a950
Alex <argeares97@gmail.com>
2024-06-14 16:20:22 +0300

    fix: styles

--- apps/docs/src/app/icons/page.tsx
+++ apps/docs/src/app/icons/page.tsx
@@ -5,9 +5,9 @@ import assets from "../../../../../icons/icons.json";
 
 const IconsPage = () => {
   return (
-    <main className="p-24">
+    <main className="p-4 md:p-8 xl:p-16">
       <h2 className="mb-8 font-bold text-xl">Assets icons list:</h2>
-      <div className="gap-2 grid grid-cols-10">
+      <div className="gap-2 grid grid-auto-cols grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 auto-rows-auto">
         {assets.map((asset) => (
           <IconCard key={asset.symbol} name={asset.name} symbol={asset.symbol}>
             <AssetIcon symbol={asset.symbol} variant="full" />
