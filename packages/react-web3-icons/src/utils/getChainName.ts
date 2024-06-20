044e8bc952b75bbb13579145d2dcb84e563f2a75
Alex <argeares97@gmail.com>
2024-06-20 09:41:26 +0300

    fix: package type

--- packages/react-web3-icons/src/utils/getChainName.ts
+++ packages/react-web3-icons/src/utils/getChainName.ts
@@ -1,4 +1,4 @@
-import { ChainType } from "../../../../src/scripts/types";
+import { ChainType } from "../types";
 import { chainsNames } from "./chainsNames";
 export const getChainName = ({ chainId }: Pick<ChainType, "chainId">) => {
   return chainsNames[chainId] ?? "Unknown";
