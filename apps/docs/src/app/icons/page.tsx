325303728e53ad0a13555bf8f17556a15e46e64e
Alex <argeares97@gmail.com>
2024-07-11 07:53:47 +0300

    fix: update icons

--- apps/docs/src/app/icons/page.tsx
+++ apps/docs/src/app/icons/page.tsx
@@ -9,39 +9,50 @@ import { ChainIconCard } from "@/components/ChainIconCard";
 import icons from "../../../../../icons/icons.json";
 import { IconType } from "../../../../../src/scripts/types";
 
-const IconsPage = () => {
+async function IconsPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
   return (
     <main className="p-4 md:p-8 xl:p-16">
       <div className="flex flex-wrap items-center justify-center gap-2 sm:justify-start">
-        {icons.map((item) => {
-          const asset = item;
-          if (asset.type.includes(IconType.asset)) {
-            return (
-              <AssetIconCard
-                key={asset?.symbol ?? asset?.chainId}
-                name={asset.name ?? ""}
-                symbol={asset?.symbol ?? ""}
-                chainId={asset.chainId}
-                chainName={asset.chainName}
-                icons={asset.icons}
-              />
-            );
-          } else {
-            return (
-              <ChainIconCard
-                key={asset.chainId}
-                chainId={asset.chainId ?? 1}
-                name={asset.chainName ?? asset.name ?? "Unknown"}
-                iconPath={asset.icons.mono}
-              />
-            );
-          }
-        })}
+        {icons
+          .filter((item) => item.name !== "Unknown")
+          .map((item) => {
+            const asset = item;
+            if (asset.type.includes(IconType.asset)) {
+              return (
+                <AssetIconCard
+                  key={asset?.symbol ?? asset?.chainId}
+                  name={asset.name ?? ""}
+                  symbol={asset?.symbol ?? ""}
+                  chainId={asset.chainId}
+                  chainName={asset.chainName}
+                  icons={asset.icons}
+                />
+              );
+            } else {
+              return (
+                <ChainIconCard
+                  key={asset.chainId}
+                  chainId={asset.chainId ?? 1}
+                  name={asset.chainName ?? asset.name ?? "Unknown"}
+                  iconPath={asset.icons.mono}
+                />
+              );
+            }
+          })}
       </div>
 
       <Branding />
     </main>
   );
-};
+}
 
 export default IconsPage;
