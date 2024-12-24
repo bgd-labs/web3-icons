4f83059607a76639de1606025ad2548d5dc7474f
Aleksei <gorbunovalexeyleti@gmail.com>
2024-12-24 09:48:08 +0100

    Add AUSD asset icon

---
+++ packages/react-web3-icons/src/components/icons/mono/StataausdIcon.tsx
@@ -0,0 +1,18 @@
+// GENERATE BY ./src/scripts/generateComponents.ts
+// DO NOT EDIT IT MANUALLY
+
+import React from "react";
+
+import { iconStataausd } from "../../../icons/mono/build/icon-stataausd.icon";
+import { IconComponentBaseProps } from "../../../utils";
+import { Image } from "../../Base/Image";
+
+const StataausdIcon = (props: IconComponentBaseProps) => {
+  return <Image svgCode={iconStataausd.data} {...props} />;
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  StataausdIcon.displayName = 'StataausdIcon';
+}
+
+export default StataausdIcon;
